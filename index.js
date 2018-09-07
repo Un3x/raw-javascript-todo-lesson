83b2f9d85a1191eaf65b4f25c385c2c5b51cef42
thomas.comes <thomas.comes@matters.tech>
2018-09-07 17:29:26 +0200

    Step 4: Adding validation and validation message

--- index.js
+++ index.js
@@ -2,10 +2,29 @@
     var input = $('#userInput');
     var submit = $('#submitTodo');
     var ul = $('#ul');
+    var error = $('#error');
+
+    function isInputValid() {
+        return input.val().length > 0;
+    }
+
+    function hideError() {
+        error.hide();
+    }
+
+    function displayError() {
+        error.show();
+    }
 
     function addTodo() {
-        var li = $('<li class="list-group-item">' + input.val() + '</li>');
-        ul.append(li);
+        if (isInputValid()) {
+            var li = $('<li class="list-group-item">' + input.val() + '</li>');
+            ul.append(li);
+            input.val('');
+            hideError();
+        } else {
+            displayError();
+        }
     }
 
     submit.click(addTodo);
